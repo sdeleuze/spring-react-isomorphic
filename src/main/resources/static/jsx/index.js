aca02f9e9e4d76ce271d0ed7c92b30f94da3bcad
Rory Hunter <rory.hunter@blackpepper.co.uk>
2015-12-07 14:45:05 +0000

    Silence React deprecation warnings

--- src/main/resources/static/jsx/index.js
+++ src/main/resources/static/jsx/index.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React          from 'react';
+import ReactDOM       from 'react-dom';
+import ReactDOMServer from 'react-dom/server';
+
 import CommentForm from './comment-form';
 import CommentList from './comment-list';
 
 if (typeof window !== 'undefined') {
-  React.render(<CommentForm onCommentSubmit={ function(comment) {
+  ReactDOM.render(<CommentForm onCommentSubmit={ function(comment) {
    $.post('/', comment, null, 'json');
   } }/>, document.getElementById("navbar"));
 
   $.getJSON('/', function( data ) {
-    React.render(<CommentList comments={ data }/>, document.getElementById("comments"));
+    ReactDOM.render(<CommentList comments={ data }/>, document.getElementById("comments"));
   });
 }
 
 function renderCommentList(comments) {
-  return React.renderToString(React.createElement(CommentList, { comments: comments} ));
+  return ReactDOMServer.renderToString(React.createElement(CommentList, { comments: comments} ));
 }
 
 function renderCommentForm() {
-  return React.renderToString(React.createElement(CommentForm));
+  return ReactDOMServer.renderToString(React.createElement(CommentForm));
 }
 
 module.exports = {
